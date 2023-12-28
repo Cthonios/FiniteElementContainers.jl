e845eaa479d5e33f34f07a332d1c06febf996764
Craig M. Hamel <cmhamel32@gmail.com>
2023-12-28 11:54:41 -0500

    patching up vectorized nodal field to handle structarrays correctly. Mainly consisted of specializing methods either to structarrrays or the abstract type dense array which is the supertype of matrix and vector.

--- src/fields/VectorizedNodalField.jl
+++ src/fields/VectorizedNodalField.jl
@@ -5,20 +5,22 @@ struct VectorizedNodalField{
 end
 
 Base.IndexStyle(::Type{<:VectorizedNodalField}) = IndexLinear()
-Base.axes(field::VectorizedNodalField) = (Base.OneTo(num_fields(field)), Base.OneTo(num_nodes(field)))
+Base.axes(::VectorizedNodalField{T, N, NF, NN, V}) where {T, N, NF, NN, V <: DenseArray} = (Base.OneTo(NF), Base.OneTo(NN))
+Base.axes(::VectorizedNodalField{T, N, NF, NN, V}) where {T, N, NF, NN, V <: StructArray}   = (Base.OneTo(NN),)
 Base.getindex(field::VectorizedNodalField, n::Int) = getindex(field.vals, n)
-function Base.getindex(field::VectorizedNodalField, d::Int, n::Int) 
+function Base.getindex(field::VectorizedNodalField{T, N, NF, NN, V}, d::Int, n::Int) where {T, N, NF, NN, V <: DenseArray}
   @assert d > 0 && d <= num_fields(field)
   @assert n > 0 && n <= num_nodes(field)
   getindex(field.vals, (n - 1) * num_fields(field) + d)
 end
 Base.setindex!(field::VectorizedNodalField, v, n::Int) = setindex!(field.vals, v, n)
-function Base.setindex!(field::VectorizedNodalField, v, d::Int, n::Int)
+function Base.setindex!(field::VectorizedNodalField{T, N, NF, NN, V}, v, d::Int, n::Int) where {T, N, NF, NN, V <: DenseArray}
   @assert d > 0 && d <= num_fields(field)
   @assert n > 0 && n <= num_nodes(field)
   setindex!(field.vals, v, (n - 1) * num_fields(field) + d)
 end
-Base.size(::VectorizedNodalField{T, N, NF, NN, V}) where {T, N, NF, NN, V} = (NF, NN)
+Base.size(::VectorizedNodalField{T, N, NF, NN, V}) where {T, N, NF, NN, V <: DenseArray} = (NF, NN)
+Base.size(::VectorizedNodalField{T, N, NF, NN, V}) where {T, N, NF, NN, V <: StructArray}   = (NN,)
 
 function VectorizedNodalField{NF, NN}(vals::V) where {NF, NN, V <: AbstractArray{<:Number, 1}}
   @assert length(vals) == NF * NN
