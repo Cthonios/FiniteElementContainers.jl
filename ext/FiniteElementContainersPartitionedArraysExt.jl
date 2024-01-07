52d4ddb9dd2a87f67a130e596dcec44f671f999e
Craig M. Hamel <cmhamel32@gmail.com>
2024-01-07 09:19:18 -0500

    adding an initial skeleton for a PartitionedArrays extensions.

---
+++ ext/FiniteElementContainersPartitionedArraysExt.jl
@@ -0,0 +1,13 @@
+module FiniteElementContainersPartitionedArraysExt
+
+using FiniteElementContainers
+using PartitionedArrays
+
+# TODO
+# 1. Probably need a parallel file mesh type
+# 2. want a way to setup nodal coordinates across devices with appropriate ghosting
+# 3. Make NodalFields work with PartitionedArrays
+# 4. Need the DofManager to work with PartitionedArrays
+# 5
+
+end # module
