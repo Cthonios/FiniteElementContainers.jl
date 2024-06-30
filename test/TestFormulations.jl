b3d2b9ee1e95bad698548deb179b2248cc597d0a
Craig M. Hamel <cmhamel32@gmail.com>
2024-06-30 06:55:22 -0400

    first attempt at unit testing discrete gradients

--- test/TestFormulations.jl
+++ test/TestFormulations.jl
@@ -1,4 +1,4 @@
-function test_plane_strain(∇N_x, ∇u_q, A_q)
+function test_plane_strain(∇N_X, ∇u_q, A_q)
   form = PlaneStrain()
   ∇u_sm = modify_field_gradients(form, ∇u_q, SMatrix)
   @test ∇u_sm ≈ SMatrix{3, 3, Float64, 9}((
@@ -14,12 +14,68 @@ function test_plane_strain(∇N_x, ∇u_q, A_q)
   ))
   P_vec = extract_stress(form, ∇u_t)
   @test P_vec ≈ SVector{4, Float64}((1., 2., 3., 4.))
-  # A_mat = extract_stiffness(form, A_q)
-  # @test A_mat[1, 1] ≈ 1.0
-  # @test A_mat[1, 2] ≈ 9.0
-  # @test A_mat[1, 3] ≈ 6.0
-  # @test A_mat[1, 4] ≈ 2.0
-end
+  A_mat = extract_stiffness(form, A_q)
+  @test A_mat[1, 1] ≈ 1.0
+  @test A_mat[1, 2] ≈ 2.0
+  @test A_mat[1, 3] ≈ 4.0
+  @test A_mat[1, 4] ≈ 5.0
+  #
+  @test A_mat[2, 1] ≈ 10.0
+  @test A_mat[2, 2] ≈ 11.0
+  @test A_mat[2, 3] ≈ 13.0
+  @test A_mat[2, 4] ≈ 14.0
+  #
+  @test A_mat[3, 1] ≈ 28.0
+  @test A_mat[3, 2] ≈ 29.0
+  @test A_mat[3, 3] ≈ 31.0
+  @test A_mat[3, 4] ≈ 32.0
+  #
+  @test A_mat[4, 1] ≈ 37.0
+  @test A_mat[4, 2] ≈ 38.0
+  @test A_mat[4, 3] ≈ 40.0
+  @test A_mat[4, 4] ≈ 41.0
+
+  G = discrete_gradient(form, ∇N_X)
+  @test G[1, 1] ≈ ∇N_X[1, 1]
+  @test G[1, 2] ≈ 0.0
+  @test G[1, 3] ≈ ∇N_X[1, 2]
+  @test G[1, 4] ≈ 0.0
+  #
+  @test G[2, 1] ≈ 0.0
+  @test G[2, 2] ≈ ∇N_X[1, 1]
+  @test G[2, 3] ≈ 0.0
+  @test G[2, 4] ≈ ∇N_X[1, 2]
+  #
+  @test G[3, 1] ≈ ∇N_X[2, 1]
+  @test G[3, 2] ≈ 0.0
+  @test G[3, 3] ≈ ∇N_X[2, 2]
+  @test G[3, 4] ≈ 0.0
+  #
+  @test G[4, 1] ≈ 0.0
+  @test G[4, 2] ≈ ∇N_X[2, 1]
+  @test G[4, 3] ≈ 0.0
+  @test G[4, 4] ≈ ∇N_X[2, 2]
+  # half
+  @test G[5, 1] ≈ ∇N_X[3, 1]
+  @test G[5, 2] ≈ 0.0
+  @test G[5, 3] ≈ ∇N_X[3, 2]
+  @test G[5, 4] ≈ 0.0
+  #
+  @test G[6, 1] ≈ 0.0
+  @test G[6, 2] ≈ ∇N_X[3, 1]
+  @test G[6, 3] ≈ 0.0
+  @test G[6, 4] ≈ ∇N_X[3, 2]
+  #
+  @test G[7, 1] ≈ ∇N_X[4, 1]
+  @test G[7, 2] ≈ 0.0
+  @test G[7, 3] ≈ ∇N_X[4, 2]
+  @test G[7, 4] ≈ 0.0
+  #
+  @test G[8, 1] ≈ 0.0
+  @test G[8, 2] ≈ ∇N_X[4, 1]
+  @test G[8, 3] ≈ 0.0
+  @test G[8, 4] ≈ ∇N_X[4, 2]
+end 
 
 function test_three_dimensional(∇N_X, ∇u_q, A_q)
   form = ThreeDimensional()
@@ -33,6 +89,7 @@ function test_three_dimensional(∇N_X, ∇u_q, A_q)
   @test P_vec ≈ SVector{9, Float64}((1, 2, 3, 4, 5, 6, 7, 8, 9))
 end
 
+# TODO test formulations on various element types
 @testset ExtendedTestSet "Formulations" begin
   X = [
     0.0 0.0;
