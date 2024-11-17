5f943eb8409d8db92862f13d93d24d27b2386eea
Craig M. Hamel <cmhamel32@gmail.com>
2024-11-16 22:17:42 -0500

    fixing issues in large allocations for elements with nodes with larger than 4 vertices.

--- src/formulations/ThreeDimensional.jl
+++ src/formulations/ThreeDimensional.jl
@@ -10,58 +10,59 @@ $(TYPEDSIGNATURES)
 """
 function discrete_gradient(::ThreeDimensional, ∇N_X)
   N   = size(∇N_X, 1)
-  tup = ntuple(i -> 0.0, Val(9 * 3 * N))
+  # tup = ntuple(i -> 0.0, Val(9 * 3 * N))
+  tup = zeros(SVector{9 * 3 * N, eltype(∇N_X)})
 
   for n in 1:N
     k = 3 * (n - 1) 
-    tup = set(tup, ∇N_X[n, 1], k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, ∇N_X[n, 1], k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, ∇N_X[n, 1], k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, ∇N_X[n, 1], k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 2 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, ∇N_X[n, 1], k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, ∇N_X[n, 1], k + 3)
 
     #
     k = 3 * (n - 1) + 3 * 3 * N
-    tup = set(tup, ∇N_X[n, 2], k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, ∇N_X[n, 2], k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 4 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, ∇N_X[n, 2], k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, ∇N_X[n, 2], k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 5 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, ∇N_X[n, 2], k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, ∇N_X[n, 2], k + 3)
 
     #
     k = 3 * (n - 1) + 6 * 3 * N
-    tup = set(tup, ∇N_X[n, 3], k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, ∇N_X[n, 3], k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 7 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, ∇N_X[n, 3], k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, ∇N_X[n, 3], k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 8 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, ∇N_X[n, 3], k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, ∇N_X[n, 3], k + 3)
   end
 
-  return SMatrix{3 * N, 9, eltype(∇N_X), 3 * N * 9}(tup)
+  return SMatrix{3 * N, 9, eltype(∇N_X), 3 * N * 9}(tup.data)
 end
 
 """
@@ -69,41 +70,42 @@ $(TYPEDSIGNATURES)
 """
 function discrete_symmetric_gradient(::ThreeDimensional, ∇N_X)
   N   = size(∇N_X, 1)
-  tup = ntuple(i -> 0.0, Val(6 * 3 * N))
+  # tup = ntuple(i -> 0.0, Val(6 * 3 * N))
+  tup = zeros(SVector{6 * 3 * N, eltype(∇N_X)})
 
   for n in 1:N
     k = 3 * (n - 1)
-    tup = set(tup, ∇N_X[n, 1], k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, ∇N_X[n, 1], k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, ∇N_X[n, 2], k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, ∇N_X[n, 2], k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 2 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, ∇N_X[n, 3], k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, ∇N_X[n, 3], k + 3)
 
     k = 3 * (n - 1) + 3 * 3 * N
-    tup = set(tup, ∇N_X[n, 2], k + 1)
-    tup = set(tup, ∇N_X[n, 1], k + 2)
-    tup = set(tup, 0.0,        k + 3)
+    tup = setindex(tup, ∇N_X[n, 2], k + 1)
+    tup = setindex(tup, ∇N_X[n, 1], k + 2)
+    tup = setindex(tup, 0.0,        k + 3)
 
     k = 3 * (n - 1) + 4 * 3 * N
-    tup = set(tup, 0.0,        k + 1)
-    tup = set(tup, ∇N_X[n, 3], k + 2)
-    tup = set(tup, ∇N_X[n, 2], k + 3)
+    tup = setindex(tup, 0.0,        k + 1)
+    tup = setindex(tup, ∇N_X[n, 3], k + 2)
+    tup = setindex(tup, ∇N_X[n, 2], k + 3)
 
     k = 3 * (n - 1) + 5 * 3 * N
-    tup = set(tup, ∇N_X[n, 3], k + 1)
-    tup = set(tup, 0.0,        k + 2)
-    tup = set(tup, ∇N_X[n, 1], k + 3)
+    tup = setindex(tup, ∇N_X[n, 3], k + 1)
+    tup = setindex(tup, 0.0,        k + 2)
+    tup = setindex(tup, ∇N_X[n, 1], k + 3)
 
   end
-  return SMatrix{3 * N, 6, eltype(∇N_X), 3 * N * 6}(tup)
+  return SMatrix{3 * N, 6, eltype(∇N_X), 3 * N * 6}(tup.data)
 end
 
 """
@@ -111,7 +113,8 @@ $(TYPEDSIGNATURES)
 """
 function discrete_values(::ThreeDimensional, N)
   N_nodes = size(N, 1)
-  tup = ntuple(i -> 0.0, Val(3 * N_nodes))
+  # tup = ntuple(i -> 0.0, Val(3 * N_nodes))
+  tup = zeros(SVector{3 * N_nodes, eltype(N)})
 
   for n in 1:N_nodes
     tup = setindex(tup, N[n], n)
@@ -125,7 +128,8 @@ function discrete_values(::ThreeDimensional, N)
     tup = setindex(tup, N[n], n + 2 * N_nodes)
   end
 
-  return SVector{3 * N_nodes, eltype(N)}(tup)
+  # return SVector{3 * N_nodes, eltype(N)}(tup)
+  return tup
 end
 
 """
