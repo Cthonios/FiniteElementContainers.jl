2fee9fa3f1940f639e06f90a81ae95b7b56c31b3
Craig M. Hamel <cmhamel32@gmail.com>
2025-03-29 22:53:08 -0400

    adding amdgpu ext skeleton.

---
+++ ext/FiniteElementContainersAMDGPUExt.jl
@@ -0,0 +1,32 @@
+module FiniteElementContainersAMDGPUExt
+
+using Adapt
+using AMDGPU
+using FiniteElementContainers
+using KernelAbstractions
+
+# need to double check if it's ROCArray
+FiniteElementContainers.gpu(x) = Adapt.adapt_structure(ROCArray, x)
+
+# this method need to have the assembler initialized first
+# if the stored values in asm.pattern.cscnzval or zero
+# AMDGPU will error out
+function AMDGPU.rocSPARSE.ROCSparseMatrixCSC(asm::SparseMatrixAssembler)
+  # Not sure if the line below is right on AMD
+  # @assert typeof(get_backend(asm)) <: AMDGPUBackend "Assembler is not on a AMDGPU device"
+  @assert length(asm.pattern.cscnzval) > 0 "Need to assemble the assembler once with SparseArrays.sparse!(assembler)"
+  @assert all(x -> x != zero(eltype(asm.pattern.cscnzval)), asm.pattern.cscnzval) "Need to assemble the assembler once with SparseArrays.sparse!(assembler)"
+  n_dofs = FiniteElementContainers.num_unknowns(asm.dof)
+  return AMDGPU.rocSPARSE.ROCSparseMatrixCSC(
+    asm.pattern.csccolptr,
+    asm.pattern.cscrowval,
+    asm.pattern.cscnzval,
+    (n_dofs, n_dofs)
+  )
+end
+
+function FiniteElementContainers._stiffness(asm::SparseMatrixAssembler, ::Backend)
+  return AMDGPU.rocSPARSE.ROCSparseMatrixCSC(asm)
+end
+
+end # module
