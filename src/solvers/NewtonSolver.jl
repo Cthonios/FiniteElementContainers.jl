add37f4a482c571d9ad56d99b2a90cc849d25624
Alejandro Mota <amota@sandia.gov>
2026-03-13 10:05:57 -0700

    Add optional log_callback to NewtonSolver

--- src/solvers/NewtonSolver.jl
+++ src/solvers/NewtonSolver.jl
@@ -1,14 +1,17 @@
-struct NewtonSolver{F, L, T} <: AbstractNonLinearSolver{L, T}
+struct NewtonSolver{F, L, T, CB} <: AbstractNonLinearSolver{L, T}
   max_iters::Int
   abs_increment_tol::F
   abs_residual_tol::F
   rel_residual_tol::F
   linear_solver::L
   timer::T
+  # Optional callback: (iter, norm_ΔUu, norm_R, rel_R, converged) -> nothing
+  # Called after each Newton iteration. Set to `nothing` to disable.
+  log_callback::CB
 end
 
 function NewtonSolver(linear_solver)
-  return NewtonSolver(10, 1e-12, 1e-12, 1e-12, linear_solver, linear_solver.timer)
+  return NewtonSolver(10, 1e-12, 1e-12, 1e-12, linear_solver, linear_solver.timer, nothing)
 end
 
 function solve!(solver::NewtonSolver, Uu, p)
@@ -27,13 +30,15 @@ function solve!(solver::NewtonSolver, Uu, p)
         if n == 1
           initial_norm[] = norm_R
         end
-        rel_R = initial_norm[] > 0.0 ? norm_R / initial_norm[] : norm_R
+        rel_R     = initial_norm[] > 0.0 ? norm_R / initial_norm[] : norm_R
+        converged = norm_ΔUu < solver.abs_increment_tol ||
+                    norm_R   < solver.abs_residual_tol   ||
+                    rel_R    < solver.rel_residual_tol
         @debug "Newton" n norm_ΔUu norm_R rel_R
-        if norm_ΔUu < solver.abs_increment_tol ||
-           norm_R   < solver.abs_residual_tol   ||
-           rel_R    < solver.rel_residual_tol
-          break
+        if !isnothing(solver.log_callback)
+          solver.log_callback(n, norm_ΔUu, norm_R, rel_R, converged)
         end
+        converged && break
       end
 
       KA.synchronize(KA.get_backend(solver))
