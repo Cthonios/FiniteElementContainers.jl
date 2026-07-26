7c5464b2135986335d84771ba7f91be029676039
Craig M. Hamel <cmhamel32@gmail.com>
2026-07-26 08:28:12 -0400

    removing old unused property interface then was never completed.

--- src/FiniteElementContainers.jl
+++ src/FiniteElementContainers.jl
@@ -52,7 +52,6 @@ export H1Field
 export HcurlField
 export HdivField
 export L2Field
-export Properties
 export StateVariableField
 export num_entities
 export num_fields
@@ -223,7 +222,6 @@ include("InitialConditions.jl")
 
 include("Formulations.jl")
 include("Physics.jl")
-include("Properties.jl")
 include("assemblers/Assemblers.jl")
 #
 include("TimeSteppers.jl")
