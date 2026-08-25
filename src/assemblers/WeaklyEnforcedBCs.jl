92c1e06d8a36a0da9cbabd5f343d74d811e9b516
Craig M. Hamel <cmhamel32@gmail.com>
2026-08-25 19:24:58 -0400

    Initial simple error checking. Checks that block sizes are between 1 and 1024. Currently doesn't check for multiple of wave front since this could be device specific. Will make more granular in the future. Also supports parsing string values from LocalPreferences.jl and added simple error checking unit tests.

--- src/assemblers/WeaklyEnforcedBCs.jl
+++ src/assemblers/WeaklyEnforcedBCs.jl
@@ -65,7 +65,7 @@ function _assemble_block_vector_weakly_enforced_bc!(
   U::AbstractField, X::AbstractField,
   conns, ref_fe, sides, vals
 )
-  fec_foreach(sides; block_size = ASSEMBLE_VECTOR_WEAKLY_ENFORCE_BC_GPU_BLOCK_SIZE) do e
+  fec_foreach(sides; block_size = ASSEMBLE_VECTOR_WEAKLY_ENFORCED_BC_GPU_BLOCK_SIZE) do e
     side = sides[e]
     conn = connectivity(ref_fe, conns, e, 1) # 1 for coffset
     surf_conns = surface_connectivity(ref_fe, conns, side, e, 1) # 1 for coffset
